0375e60bbc835164f8a5ecaaf2a8aee33431bfe5
Alexander Malukhin <keeonenew@yandex.ru>
2023-06-23 14:15:17 +0300

    feat: patch user avatar

--- controllers/users.js
+++ controllers/users.js
@@ -108,9 +108,40 @@ const updateUser = (req, res) => {
     })
 }
 
+const updateUserAvatar = (req, res) => {
+  const { avatar } = req.body;
+  const owner = req.user._id;
+
+  User.findByIdAndUpdate(owner, { avatar }, { new: true, runValidators: true })
+    .then((user) => {
+      if (!user) {
+        res.status(NOT_FOUND_ERROR)
+          .send({
+            message: "Данные не найдены"
+          });
+      }
+    })
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res.status(BAD_REQUEST_ERROR)
+          .send({
+            message: "Данные переданы не правильно"
+          });
+      } else {
+        res.status(INTERNAL_SERVER_ERROR)
+          .send({
+            message: "Ошибка сервера",
+            err: err.message,
+            stack: err.stack,
+          })
+      }
+    })
+}
+
 module.exports = {
   getUsers,
   getUserById,
   createUser,
   updateUser,
+  updateUserAvatar,
 };
--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,6 @@ router.get("/users", getUsers);
 router.get("/users/:userId", getUserById);
 router.post("/users", createUser);
 router.patch("/users/me", updateUser);
-// router.patch("/users/me/avatar", updateUserAvatar)
+router.patch("/users/me/avatar", updateUserAvatar)
 
 module.exports = router;
