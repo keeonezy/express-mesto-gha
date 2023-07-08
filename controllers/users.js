7e86364555bf10f70e0bb62b5f0c6cdfe7149edb
Alexander Malukhin <keeonenew@yandex.ru>
2023-07-08 19:14:37 +0300

    feat: add check status 409

--- controllers/users.js
+++ controllers/users.js
@@ -4,6 +4,7 @@ const User = require('../models/user');
 const { STATUS_CREATED } = require('../utils/responseStatus');
 const BadRequestError = require('../utils/status-400');
 const NotFoundError = require('../utils/status-404');
+const ConflictError = require("../utils/status-409")
 
 module.exports.getUsers = async (req, res, next) => {
   try {
@@ -58,6 +59,38 @@ module.exports.createUser = async (req, res, next) => {
   } catch (err) {
     if (err.name === 'ValidationError') {
       next(new BadRequestError('Не правильно переданы данные'));
+    } else if (err.code === 11000) {
+      next(new ConflictError('Пользователь с такой почтой уже зарегистрирован'));
+    } else {
+      next(err);
+    }
+  }
+};
+
+module.exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    // Существует ли email
+    const user = await User.findUserByCredentials(email, password);
+    if (user) {
+      const isValidUser = await bcrypt.compare(String(password), user.password);
+      if (isValidUser) {
+        // Создаем JWT токен. В process.env передаем наш код секретный
+        const token = jwt.sign({ _id: user._id }, process.env.SECRET__HEHE);
+        // закреляем JWT к cookie
+        res.cookie('jwt', token, {
+          maxAge: 7 * 24 * 60 * 60 * 1000, // 7 дней(дни, часы, минуты, секунды, милисекунды)
+          httpOnly: true, // Cookie только для http запроса, а не js
+          sameSite: true, // Cookie отправляется только в рамках 1 домена
+          secure: true, // Cookie только для https соединения
+        });
+        res.send({ token });
+      }
+    }
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      next(new BadRequestError('Не правильно переданы данные'));
     } else {
       next(err);
     }
@@ -104,34 +137,4 @@ module.exports.updateUserAvatar = (req, res, next) => {
         next(err);
       }
     });
-};
-
-module.exports.login = async (req, res, next) => {
-  const { email, password } = req.body;
-
-  try {
-    // Существует ли email
-    const user = await User.findUserByCredentials(email, password);
-    if (user) {
-      const isValidUser = await bcrypt.compare(String(password), user.password);
-      if (isValidUser) {
-        // Создаем JWT токен. В process.env передаем наш код секретный
-        const token = jwt.sign({ _id: user._id }, process.env.SECRET__HEHE);
-        // закреляем JWT к cookie
-        res.cookie('jwt', token, {
-          maxAge: 7 * 24 * 60 * 60 * 1000, // 7 дней(дни, часы, минуты, секунды, милисекунды)
-          httpOnly: true, // Cookie только для http запроса, а не js
-          sameSite: true, // Cookie отправляется только в рамках 1 домена
-          secure: true, // Cookie только для https соединения
-        });
-        res.send({ token });
-      }
-    }
-  } catch (err) {
-    if (err.name === 'ValidationError') {
-      next(new BadRequestError('Не правильно переданы данные'));
-    } else {
-      next(err);
-    }
-  }
 };
